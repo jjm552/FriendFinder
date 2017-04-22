adc734112643f4e62302556447e7c3bd2ca5929b
Jeff <mcmahan.jeff@gmail.com>
2017-04-21 22:29:25 -0500

    apiRoutes - working logic, survey- modal working/diplays match, friends-more data added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 var friendArray = [{
-    name: "Ahmed",
+    name: "Arthur",
     photo: "http://www-tc.pbskids.org/arthur/health/img/resilience_home_1.png",
     scores: [
         5,
@@ -11,6 +11,51 @@ var friendArray = [{
         2,
         5,
         4,
+        1
+    ]
+}, {
+    name: "Francine",
+    photo: "http://static.tvtropes.org/pmwiki/pub/images/francine_3681.png",
+    scores: [
+        1,
+        5,
+        3,
+        3,
+        1,
+        5,
+        4,
+        1,
+        3,
+        5
+    ]
+}, {
+    name: "Buster",
+    photo: "http://www-tc.pbskids.org/arthur/health/img/asthma_quiz.png",
+    scores: [
+        1,
+        2,
+        2,
+        3,
+        3,
+        3,
+        4,
+        4,
+        5,
+        5
+    ]
+}, {
+    name: "Muffy",
+    photo: "http://images2.wikia.nocookie.net/__cb20130226081254/arthur/images/7/7a/Muffy.png",
+    scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1
     ]
 }];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,14 @@
 
 var friends = require("../data/friends");
 
+// Friend constructor used in API POST request to build new friends
+function FriendConstruct(name, photo, scores) {
+    this.name = name;
+    this.photo = photo;
+    this.scores = scores;
+}
+
+
 // ROUTING
 module.exports = function(app) {
 
@@ -12,6 +20,38 @@ module.exports = function(app) {
 
     // API POST request
     app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
+        userEntry = req.body;
+        newFriend = new FriendConstruct(userEntry.name, userEntry.photo, userEntry.scores);
+        friends.push(newFriend);
+
+        // arry for compaing current user scores to stored scores
+        var friendMatch = [];
+
+        // loop through friends array
+        for (var i = 0; i < friends.length - 1; i++) {
+
+            var totalDiff = 0;
+            // loop throught the scores of each friend and compare to user
+            for (var j = 0; j < newFriend.scores.length; j++) {
+                if (newFriend.scores[j] != friends[i].scores[j]) {
+                    totalDiff += Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+                }
+            }
+            console.log(totalDiff);
+            friendMatch.push(totalDiff);
+        }
+        // displays total difference of each stored friened compared to user and removes user scores for match process
+        console.log("match: " + friendMatch);
+        friendMatch.pop();
+        // function to return smallest value in an array
+        Array.min = function(array) {
+            return Math.min.apply(Math, array);
+        };
+        // retunrs the index of best match / firend with the least total difference
+        var min = friendMatch.indexOf(Array.min(friendMatch));
+        console.log(min);
+        // uses the min from above for index of friends array to send front end
+        res.json(friends[min]);
+
     });
 };
